51c913851035f5fc9cd987532cd2d27dcea42ccf
Mariana Stocco <mariana.stocco@outlook.com>
2022-08-06 09:53:38 -0300

    V1

---
+++ src/components/Skills.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Css from '../assets/css.png';
+import Javascript from '../assets/javascript.png';
+import Html from '../assets/html.png';
+import Node from '../assets/nodejs.png';
+import ReactJs from '../assets/react.png';
+import Postgres from '../assets/postgres.png';
+import Github from '../assets/gitHub.png';
+import Tailwind from '../assets/tailwind.png';
+import Redux from '../assets/redux.png'
+
+export const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Css} alt="CSS icon" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Javascript} alt="Js icon" />
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Html} alt="HTML icon" />
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Node} alt="NodeJs icon" />
+                    <p className='my-4'>NODE JS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={ReactJs} alt="React icon" />
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Postgres} alt="Postgres icon" />
+                    <p className='my-4'>POSTGRES</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Github} alt="GitHub icon" />
+                    <p className='my-4'>GITHUB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Redux} alt="Redux icon" />
+                    <p className='my-4'>REDUX</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto'src={Tailwind} alt="Tailwind icon" />
+                    <p className='my-4'>TAILWIND</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
